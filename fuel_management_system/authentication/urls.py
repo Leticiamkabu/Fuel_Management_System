17a23644e736a6486d61ec8cf84a72d6c00ecc16
Leticiamkabu <kabuleticia36@gmail.com>
2023-10-20 21:41:04 +0000

    did one changes

--- fuel_management_system/authentication/urls.py
+++ fuel_management_system/authentication/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('login/', Login_View.as_view(), name = 'Login'),
     path('users/', UserView.as_view(), name = 'users'),
     path('attendance/<str:action>/', Attendance_View.as_view(), name = 'Attendance'),
+    path('attendance/', get_attendance, name = 'Attendance'),
+    path('attendance_by_user/<int:id>/', list_of_attendance_by_id, name = 'Attendance_by_id'),
 
 ]
--- fuel_management_system/manager/urls.py
+++ fuel_management_system/manager/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('dipping/<int:id>',Fuel_dipping_view_by_id.as_view(),name = 'dipping_by_id'),
     path('fuel_prices/',Fuel_prices_view.as_view(),name = 'fuel_price'),
     path('fuel_prices/<int:id>',Fuel_prices_view_by_id.as_view(),name = 'fuel_price_by_id'),
+    # path('fuel_pricess/',get_attendnace,name = 'fuel_price_by_id'),
 ]
